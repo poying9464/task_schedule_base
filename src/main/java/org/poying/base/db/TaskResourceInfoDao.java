df12ceb8061cb7d2f4c11f7ea0620ecd633d1912
泼荧 <yian9464@qq.com>
2025-08-18 21:55:01 +0800

    feat(任务围绕增强) 增加扩展点

--- src/main/java/org/poying/base/db/TaskResourceInfoDao.java
+++ src/main/java/org/poying/base/db/TaskResourceInfoDao.java
@@ -10,16 +10,19 @@ public class TaskResourceInfoDao {
     public static void save(TaskResourcesSurround.TaskResourceInfo taskResourceInfo, DataSource dataSource) {
         // 使用JDBC保存任务资源信息
         try (Connection connection = dataSource.getConnection()) {
-            String sql = "INSERT INTO task_resource_info (task_name, execution_time, cpu_time, memory_used, max_memory_used, memory_samples) VALUES (?, ?, ?, ?, ?, ?)";
+            String sql = "INSERT INTO task_resource_info (jobKey, task_name, execution_time, cpu_time, memory_used, max_memory_used, memory_samples, create_time, update_time) VALUES (?, ?, ?, ?, ?, ?, ?)";
             try (java.sql.PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
                 // 开启事务
                 connection.setAutoCommit(false);
-                preparedStatement.setString(1, taskResourceInfo.getTaskName());
-                preparedStatement.setLong(2, taskResourceInfo.getExecutionTimeMillis());
-                preparedStatement.setLong(3, taskResourceInfo.getCpuTimeNanos());
-                preparedStatement.setLong(4, taskResourceInfo.getMemoryUsedBytes());
-                preparedStatement.setLong(5, taskResourceInfo.getMaxMemoryUsedBytes());
-                preparedStatement.setString(6, taskResourceInfo.getMemorySamples().toString());
+                preparedStatement.setString(1, taskResourceInfo.getJobKey());
+                preparedStatement.setString(2, taskResourceInfo.getTaskName());
+                preparedStatement.setLong(3, taskResourceInfo.getExecutionTimeMillis());
+                preparedStatement.setLong(4, taskResourceInfo.getCpuTimeNanos());
+                preparedStatement.setLong(5, taskResourceInfo.getMemoryUsedBytes());
+                preparedStatement.setLong(6, taskResourceInfo.getMaxMemoryUsedBytes());
+                preparedStatement.setString(7, taskResourceInfo.getMemorySamples().toString());
+                preparedStatement.setTimestamp(8, new java.sql.Timestamp(System.currentTimeMillis()));
+                preparedStatement.setTimestamp(9, new java.sql.Timestamp(System.currentTimeMillis()));
                 preparedStatement.executeUpdate();
                 connection.commit();
             } catch (SQLException e) {
